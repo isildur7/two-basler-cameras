af91a094b2e6e58b71be23d79db7636145aff76d
Amey Chaware <amey.chaware@duke.edu>
2021-11-23 18:45:53 -0500

    capture and append separately

--- video_on_one_camera.py
+++ video_on_one_camera.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from pypylon import pylon
 import imageio as iio
 
-FPS = 20.0
+FPS = 20
 RECORDING_TIME = 10  # in seconds
 NUM_IMAGES = FPS * RECORDING_TIME
 
@@ -15,31 +15,32 @@ cam.PixelFormat = "Mono8"
 
 cam.AcquisitionFrameRateEnable.SetValue(True)
 cam.AcquisitionFrameRate.SetValue(FPS)
-cam.ExposureTime.SetValue(1000000*(1/FPS) - 1100)
+cam.ExposureTime.SetValue(40000)
 
 start = datetime.now()
 
 with iio.get_writer(
-        'E:\\samplevid.avi',  # mkv players often support H.264
+        'E:\\samplevid0.avi',  # mkv players often support H.264
         fps=FPS,  # FPS is in units Hz; should be real-time.
         codec='libx264',  # When used properly, this is basically
                               # "PNG for video" (i.e. lossless)
         quality=None,  # disables variable compression
-        pixelformat='yuv420p',  # keep it as RGB colours
+        pixelformat='gray',  # keep it as RGB colours
         ffmpeg_params=[  # compatibility with older library versions
             '-preset',  # set to faster, veryfast, superfast, ultrafast
             'fast',     # for higher speed but worse compression
             '-crf',  # quality; set to 0 for lossless, but keep in mind
             '11'     # that the camera probably adds static anyway
         ]) as writer:
-    images = 0
-    cam.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
-    while images < NUM_IMAGES and cam.IsGrabbing():
+
+    cam.StartGrabbingMax(NUM_IMAGES)
+    buffer = []
+    while cam.IsGrabbing():
         res = cam.RetrieveResult(1000)
-        writer.append_data(res.Array)
+        buffer.append(res.Array)
         res.Release()
-        images += 1
-    cam.StopGrabbing()
+    for image in buffer:
+        writer.append_data(image)
 end = datetime.now()
 print("actual runtime {}".format(end - start))
 cam.Close()
